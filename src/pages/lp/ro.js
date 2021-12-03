812390a5d27e7b23752975a8d2b36703d3ec6780
Gleb Azarov <12088742+CheerlessCloud@users.noreply.github.com>
2021-12-03 13:32:55 +0300

    Changes

--- src/pages/lp/ro.js
+++ src/pages/lp/ro.js
@@ -54,6 +54,7 @@ export default (() => {
 			<script async={true} place={"endOfBody"} rawKey={"61546afc558d30d91557a10e"}>
 				{"<iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-N7L44LR\"\nheight=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>"}
 			</script>
+			<link rel={"canonical"} href={"/lp/ro/"} place={"endOfHead"} rawKey={"61a9f16011fb7a8ddc6d72bb"} />
 		</RawHtml>
 	</Theme>;
 });
